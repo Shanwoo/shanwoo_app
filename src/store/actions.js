491364f23c9068c31efadaea18f36b8bcac7ce9b
mutuyang <243507547@qq.com>
2018-10-15 17:00:47 +0800

    get token balance

--- src/store/actions.js
+++ src/store/actions.js
@@ -20,6 +20,24 @@ let actions = {
     axios.get('/api/transactions').then(res => {
       commit('addTransactions', res.data)
     })
+  },
+  registerWeb3 ({commit}) {
+    console.log('registerWeb3 Action being executed')
+    getWeb3.then(result => {
+      console.log('committing result to registerWeb3Instance mutation')
+      commit('registerWeb3Instance', result)
+    }).catch(e => {
+      console.log('error in action registerWeb3', e)
+    })
+  },
+  pollWeb3 ({commit}, payload) {
+    console.log('pollWeb3 action being executed')
+    commit('pollWeb3Instance', payload)
+  },
+  getContractInstance ({commit}) {
+    getContract.then(result => {
+      commit('registerContractInstance', result)
+    }).catch(e => console.log(e))
   }
 }
 
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -10,6 +10,27 @@ let mutations = {
   },
   addTransactions (state, payload) {
     state.transactions = payload
+  },
+  registerWeb3Instance (state, payload) {
+    console.log('registerWeb3instance Mutation being executed', payload)
+    let result = payload
+    let web3Copy = state.web3
+    web3Copy.coinbase = result.coinbase
+    web3Copy.networkId = result.networkId
+    web3Copy.balance = parseInt(result.balance, 10)
+    web3Copy.isInjected = result.injectedWeb3
+    web3Copy.web3Instance = result.web3
+    state.web3 = web3Copy
+    pollWeb3()
+  },
+  pollWeb3Instance (state, payload) {
+    console.log('pollWeb3Instance mutation being executed', payload)
+    state.web3.coinbase = payload.coinbase
+    state.web3.balance = parseInt(payload.balance, 10)
+  },
+  registerContractInstance (state, payload) {
+    console.log('Casino contract instance: ', payload)
+    state.contractInstance = () => payload
   }
 }
 
